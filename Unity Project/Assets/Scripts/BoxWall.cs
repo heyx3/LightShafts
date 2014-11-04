e44436f874c91d18eb8a36cf5f757ba462398656
heyx3 <manning.w@husky.neu.edu>
2014-11-04 15:59:15 -0500

    Initial work on assets/simple scripts

---
+++ Unity Project/Assets/Scripts/BoxWall.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// A wall that blocks the player and light.
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
+public class BoxWall : MonoBehaviour
+{
+	/// <summary>
+	/// A statically-accessible reference to all walls.
+	/// </summary>
+	public static List<BoxWall> Walls = new List<BoxWall>();
+
+
+	public Transform MyTransform { get; private set; }
+	public BoxCollider2D Box { get; private set; }
+
+
+	void Awake()
+	{
+		Box = GetComponent<BoxCollider2D>();
+		MyTransform = transform;
+
+		Walls.Add(this);
+	}
+	void OnDestroy()
+	{
+		Walls.Remove(this);
+	}
+}
