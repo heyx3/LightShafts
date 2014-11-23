22673ee6ace8e5e651fb5f216414198f13fc13dc
heyx3 <manning.w@husky.neu.edu>
2014-11-23 17:08:26 -0500

    Added more complex camera movements/zooming

--- Unity Project/Assets/Scripts/GameCamera.cs
+++ Unity Project/Assets/Scripts/GameCamera.cs
@@ -15,12 +15,17 @@ public class GameCamera : MonoBehaviour
 
 	public Camera MyCam { get; private set; }
 	public Transform MyTransform { get; private set; }
+
 	public Vector2 WorldMousePos { get; private set; }
+	public Vector2 WorldViewSize { get; private set; }
+
+	private PlayerInput Player { get { return PlayerInput.Instance; } }
+
 
 	/// <summary>
-	/// Gets and sets the resolution of the game.
-	/// If this was built with a Pro license, it resizes the game/lighting render textures.
-	/// If this was built without a Pro license, it resizes the screen itself.
+	/// Gets and sets the resolution of the game render.
+	/// If this game was built with a Pro license, it resizes the game/lighting render textures.
+	/// If this game was built without a Pro license, it resizes the screen itself.
 	/// </summary>
 	public Vector2 GameRenderSize
 	{
@@ -51,7 +56,30 @@ public class GameCamera : MonoBehaviour
 	}
 
 
+	/// <summary>
+	/// Gets and sets the orthographic size of the camera.
+	/// </summary>
+	public float CurrentZoom
+	{
+		get { return MyCam.orthographicSize; }
+		set { MyCam.orthographicSize = value; }
+	}
+
+	public float ZoomMin = 80.0f,
+				 ZoomMax = 130.0f;
+	public float ZoomMaxChangeSpeed = 0.1f;
+	public float ZoomMaxPlayerMouseDist = 1500.0f;
+
+	public float CameraSnapDist = 10.0f;
+	public Vector2 CameraBorderLerp = new Vector2(0.1f, 0.1f);
+	public float Accel = 2000.0f;
+
+	public bool IsDynamic = true;
+
 	public bool RotateWithPlayer = false;
+
+
+	[System.NonSerialized]public Vector2 Speed = Vector2.zero;
 
 
 	void Awake()
@@ -74,6 +102,8 @@ public class GameCamera : MonoBehaviour
 			MyCam.cullingMask |= (1 << LayerMask.NameToLayer("Lights"));
 			MyCam.targetTexture = null;
 		}
+
+		CurrentZoom = (ZoomMax + ZoomMin) * 0.5f;
 	}
 
 	void Update()
@@ -92,11 +122,137 @@ public class GameCamera : MonoBehaviour
 										 Mathf.Lerp(camPos.y - (cameraViewSize.y * 0.5f),
 													camPos.y + (cameraViewSize.y * 0.5f),
 													mouseLerp.y));
+
 		WorldMousePos = worldMouse;
+		WorldViewSize = cameraViewSize;
+
+
+		if (IsDynamic) UpdateZoom();
 	}
+	private void UpdateZoom()
+	{
+		float targetZoom = CurrentZoom;
+		
+		float distPlayerToMouse = Vector2.Distance(WorldMousePos, Player.MyTransform.position);
+
+		//Get the target zoom as a function of the distance.
+		float lerpDist = Mathf.Clamp(distPlayerToMouse / ZoomMaxPlayerMouseDist, 0.0f, 1.0f);
+		targetZoom = Mathf.Lerp(ZoomMin, ZoomMax, lerpDist);
+
+		//Now try to move from current zoom to target zoom.
+		if (Mathf.Abs(targetZoom - CurrentZoom) < (ZoomMaxChangeSpeed * Time.deltaTime))
+		{
+			CurrentZoom = targetZoom;
+		}
+		else
+		{
+			CurrentZoom += ZoomMaxChangeSpeed * Time.deltaTime * Mathf.Sign((targetZoom - CurrentZoom));
+		}
+
+		CurrentZoom = Mathf.Clamp(CurrentZoom, ZoomMin, ZoomMax);
+	}
+
 	void LateUpdate()
 	{
 		if (!RotateWithPlayer)
+		{
 			MyTransform.eulerAngles = new Vector3();
+		}
+
+		if (!IsDynamic)
+		{
+			MyTransform.position = new Vector3(Player.MyTransform.position.x,
+											   Player.MyTransform.position.y,
+											   MyTransform.position.z);
+			return;
+		}
+
+
+		//Update the position of the camera based on the player and mouse positions.
+
+		//If the camera is close enough, just snap to the player.
+		if (Vector2.SqrMagnitude((Vector2)MyTransform.position - WorldMousePos) <
+			(CameraSnapDist * CameraSnapDist))
+		{
+			MyTransform.position = new Vector3(WorldMousePos.x, WorldMousePos.y, MyTransform.position.z);
+			Speed = Vector2.zero;
+		}
+		//Otherwise, accelerate towards him.
+		{
+			//Get the player's position in the world view region from 0 to 1.
+			Vector2 playerViewPosLerp = (Vector2)MyCam.WorldToScreenPoint(Player.MyTransform.position);
+			playerViewPosLerp = new Vector2(playerViewPosLerp.x / GameRenderSize.x,
+											playerViewPosLerp.y / GameRenderSize.y);
+
+			Vector2 camRight = (Vector2)MyTransform.right,
+					camUp = (Vector2)MyTransform.up;
+			Vector2 toTarget = (WorldMousePos - (Vector2)MyTransform.position).normalized;
+
+			
+
+
+			//Accelerate towards the player.
+			Vector2 accel = toTarget * Accel;
+			Speed += accel * Time.deltaTime;
+
+
+			//If the player is too far to the center of the camera view, move the camera closer to him.
+
+			//Split the velocity into the individual components along the right/up vectors of the camera.
+			//One or both of these components may be zeroed out
+			//    if the camera is heading too far in one direction.
+			Vector2 speedAlongRight = camRight * Vector2.Dot(camRight, Speed),
+					speedAlongUp = Speed - speedAlongRight;
+
+			if (playerViewPosLerp.x < CameraBorderLerp.x)
+			{
+				float deltaRight = (CameraBorderLerp.x - playerViewPosLerp.x);
+				MyTransform.position -= (Vector3)(camRight * deltaRight * WorldViewSize.x);
+
+				if (Vector2.Dot(speedAlongRight, camRight) > 0.0f)
+					speedAlongRight = Vector2.zero;
+			}
+			else if (playerViewPosLerp.x > (1.0f - CameraBorderLerp.x))
+			{
+				float deltaLeft = WorldViewSize.x * (playerViewPosLerp.x - (1.0f - CameraBorderLerp.x));
+				MyTransform.position -= (Vector3)(-camRight * deltaLeft);
+
+				if (Vector2.Dot(speedAlongRight, camRight) < 0.0f)
+					speedAlongRight = Vector2.zero;
+			}
+
+			if (playerViewPosLerp.y < CameraBorderLerp.y)
+			{
+				float deltaUp = WorldViewSize.y * (CameraBorderLerp.y - playerViewPosLerp.y);
+				MyTransform.position -= (Vector3)(camUp * deltaUp);
+
+				if (Vector2.Dot(speedAlongUp, camRight) > 0.0f)
+					speedAlongUp = Vector2.zero;
+			}
+			else if (playerViewPosLerp.y > (1.0f - CameraBorderLerp.y))
+			{
+				float deltaDown = WorldViewSize.x * (playerViewPosLerp.y - (1.0f - CameraBorderLerp.y));
+				MyTransform.position -= (Vector3)(-camUp * deltaDown);
+
+				if (Vector2.Dot(speedAlongUp, camRight) < 0.0f)
+					speedAlongUp = Vector2.zero;
+			}
+
+			//Recombine the speed components.
+			Speed = speedAlongRight + speedAlongUp;
+
+
+			//Limit the speed of this camera.
+			float maxSpeed = 4.0f * Vector2.Distance((Vector2)MyTransform.position, WorldMousePos);
+			float finalSpeed;
+			if (Vector2.SqrMagnitude(Speed) > (maxSpeed * maxSpeed))
+				finalSpeed = maxSpeed;
+			else finalSpeed = Speed.magnitude;
+
+			//Redirect the velocity to point towards the target.
+			Speed = ((Vector2)WorldMousePos - (Vector2)MyTransform.position).normalized * finalSpeed;
+		}
+
+		MyTransform.position += (Vector3)Speed * Time.deltaTime;
 	}
 }
