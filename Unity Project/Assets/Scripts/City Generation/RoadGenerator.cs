99d0f6af46c8f51d02145fd21346dae4a15144fa
heyx3 <manning.w@husky.neu.edu>
2014-12-02 22:46:27 -0500

    More work on city/road generation

---
+++ Unity Project/Assets/Scripts/City Generation/RoadGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Generates the road tiles.
+/// </summary>
+[Serializable]
+public class RoadGenerator
+{
+	public Material MajorRoadMat, MinorRoadMat, AlleyMat;
+
+
+}
